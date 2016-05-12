ea1f299e09e7e2ec97d0e363ac65c79f58bad2bd
shafiq10 <shafiqakmal1994@yahoo.com>
2016-05-12 13:48:43 +0800

    Added Interface

---
+++ app/controllers/atpcalculator_controller.rb
@@ -0,0 +1,35 @@
+class AtpcalculatorController < ApplicationController
+  
+  def index
+  end
+
+  def admin
+  end
+
+  def analyze
+  end
+
+  def history
+  end
+
+  def update
+  end
+
+  def bidang
+  end
+
+  def unjuran
+  end
+
+  def lecturerload
+  end
+
+  def bidangHistory
+  end
+
+  def unjuranHistory
+  end
+
+  def lecturerHistory
+  end
+end
